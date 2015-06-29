2fc80e94f60bf6b35c788baef5bd8ff317ac91ef
sakemoto yuta <g094024r@gmail.com>
2015-06-29 11:43:23 +0800

    Hello,world!とコンソールに表示する処理を追加

---
+++ join/thanks.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta http-epuiv="Content-Type" content="text/html" charset="utf-8">
+    <title>date</title>
+  </head>
+  <body>
+	<p>ユーザー登録が完了しました</P>
+	<p><a href="../login.php">ログインする</a></p>
+  </body>
+</html>
